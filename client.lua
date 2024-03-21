004049b3ccf12371a44592e8da20602d94517df3
Straw <119638825+StrawMods@users.noreply.github.com>
2024-03-21 23:43:08 +0100

    client.lua

--- client.lua
+++ client.lua
@@ -16,51 +16,51 @@ CreateBlip = function(coords, sprite, colour, text, scale)
     EndTextCommandSetBlipName(blip)
 end
 
-AddEventHandler('ws_sellshop:sellItem', function(data)
+AddEventHandler('straw_sellshop:sellItem', function(data)
     local data = data
-    local input = lib.inputDialog('Koliko jih želite prodati?', {'Količina'})
+    local input = lib.inputDialog('How many would you like to sell?', {'Quantity'})
     if input then
         data.quantity = math.floor(tonumber(input[1]))
         if data.quantity < 1 then
             lib.notify({
-                title = 'Napaka',
-                description = 'Vnesite veljaven znesek!',
+                title = 'Error',
+                description = 'Please enter a valid amount!',
                 type = 'error'
             })
         else
-            local done = lib.callback.await('ws_sellshop:sellItem', 100, data)
+            local done = lib.callback.await('straw_sellshop:sellItem', 100, data)
             if not done then
                 lib.notify({
-                    title = 'Napaka',
-                    description = 'Nimate zahtevanih artiklov za prodajo!',
+                    title = 'Error',
+                    description = 'You have no requested items to sell!',
                     type = 'error'
                 })
             else
                 lib.notify({
-                    title = 'Uspeh',
-                    description = 'Prodali ste vaše artikle in zaslužili €'..addCommas(done),
+                    title = 'Success',
+                    description = 'You sold your goods for $'..addCommas(done),
                     type = 'success'
                 })
             end
         end
     else
         lib.notify({
-            title = 'Napaka',
-            description = 'Vnesite veljaven znesek!',
+            title = 'Error',
+            description = 'Please enter a valid amount!',
             type = 'error'
         })
     end
 end)
 
-AddEventHandler('ws_sellshop:interact', function(data)
+AddEventHandler('straw_sellshop:interact', function(data)
     local storeData = data.store
     local items = storeData.items
     local Options = {}
     for i=1, #items do
         table.insert(Options, {
             title = items[i].label,
-            description = 'Prodajna cena: €'..items[i].price,
-            event = 'ws_sellshop:sellItem',
+            description = 'Sale price: $'..items[i].price,
+            event = 'straw_sellshop:sellItem',
             args = { item = items[i].item, price = items[i].price, currency = items[i].currency }
         })
     end
@@ -84,9 +84,9 @@ CreateThread(function()
         }, {
             options = {
                 {
-                    event = 'ws_sellshop:interact',
-                    icon = 'fa-solid fa-sack-dollar',
-                    label = 'Prodaja',
+                    event = 'straw_sellshop:interact',
+                    icon = 'fas fa-hand-paper',
+                    label = 'Market',
                     store = Config.SellShops[i]
                 }
             },
@@ -97,36 +97,4 @@ CreateThread(function()
             CreateBlip(Config.SellShops[i].coords, Config.SellShops[i].blip.sprite, Config.SellShops[i].blip.color, Config.SellShops[i].label, Config.SellShops[i].blip.scale)
         end
     end
-end)
-
--- Ped spawn thread
-local pedSpawned = {}
-local pedPool = {}
-CreateThread(function()
-	while true do
-		local sleep = 1500
-        local playerPed = cache.ped
-        local pos = GetEntityCoords(playerPed)
-		for i=1, #Config.SellShops do
-			local dist = #(pos - Config.SellShops[i].coords)
-			if dist <= 20 and not pedSpawned[i] then
-				pedSpawned[i] = true
-                lib.requestModel(Config.SellShops[i].ped, 100)
-                lib.requestAnimDict('mini@strip_club@idles@bouncer@base', 100)
-				pedPool[i] = CreatePed(28, Config.SellShops[i].ped, Config.SellShops[i].coords.x, Config.SellShops[i].coords.y, Config.SellShops[i].coords.z, Config.SellShops[i].heading, false, false)
-				FreezeEntityPosition(pedPool[i], true)
-				SetEntityInvincible(pedPool[i], true)
-				SetBlockingOfNonTemporaryEvents(pedPool[i], true)
-				TaskPlayAnim(pedPool[i], 'mini@strip_club@idles@bouncer@base','base', 8.0, 0.0, -1, 1, 0, 0, 0, 0)
-			elseif dist >= 21 and pedSpawned[i] then
-				local model = GetEntityModel(pedPool[i])
-				SetModelAsNoLongerNeeded(model)
-				DeletePed(pedPool[i])
-				SetPedAsNoLongerNeeded(pedPool[i])
-                pedPool[i] = nil
-				pedSpawned[i] = false
-			end
-		end
-		Wait(sleep)
-	end
 end)
